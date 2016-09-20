b3c30619071334bd20507cae76b92e8e2b3e330a
Thomas Carli <thomas@procore.com>
2016-09-20 21:02:46 +0000

    Empty skeleton:wq

---
+++ lib/erp/logger.rb
@@ -0,0 +1,7 @@
+require "erp/logger/version"
+
+module Erp
+  module Logger
+    # Your code goes here...
+  end
+end
---
+++ spec/erp/logger_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Erp::Logger do
+  it 'has a version number' do
+    expect(Erp::Logger::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
